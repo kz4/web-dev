4468bd848f33e2ef3ee521f7a33fab58ccd3806f
kz4 <zkcneu@gmail.com>
2016-06-12 12:30:15 -0400

    rewrote $set in widget to see if widgets can be updated on openshift

--- assignment/models/widget/widget.model.server.js
+++ assignment/models/widget/widget.model.server.js
@@ -23,25 +23,26 @@ module.exports = function () {
     }
     
     function updateWidget(widgetId, widget) {
-        // return Widget.update({_id: widgetId}, {
-        //     $set:
-        //     {
-        //         name: widget.name,
-        //         text: widget.text,
-        //         placeholder: widget.placeholder,
-        //         description: widget.description,
-        //         url: widget.url,
-        //         width: widget.width,
-        //         height: widget.height,
-        //         rows: widget.rows,
-        //         size: widget.size,
-        //         class: widget.class,
-        //         icon: widget.icon,
-        //         deletable: widget.deletable,
-        //         formatted: widget.formatted
-        //     }
-        // });
-        return Widget.update({_id: widgetId}, {$set: widget});
+        return Widget.update({_id: widgetId}, {
+            $set:
+            {
+                name: widget.name || "",
+                text: widget.text || "",
+                placeholder: widget.placeholder || "",
+                description: widget.description || "",
+                url: widget.url || "",
+                width: widget.width || "",
+                height: widget.height || "",
+                rows: widget.rows || 0,
+                size: widget.size || 0,
+                class: widget.class || "",
+                icon: widget.icon || "",
+                deletable: widget.deletable || false,
+                formatted: widget.formatted || false
+            }
+        });
+
+        // return Widget.update({_id: widgetId}, {$set: widget});
     }
 
     function findWidgetById(widgetId) {
