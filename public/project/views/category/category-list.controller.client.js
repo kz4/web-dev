60bc13c007df6ff4e0bc807c7696a28e6677ade5
kz4 <zkcneu@gmail.com>
2016-06-29 01:17:15 -0400

    after submitting a comment, the textarea is now cleared. no zoom for phone view. in category-list, browser automatically detects current zip code

--- public/project/views/category/category-list.controller.client.js
+++ public/project/views/category/category-list.controller.client.js
@@ -3,7 +3,7 @@
         .module("ProjectMaker")
         .controller("CategoryListController", CategoryListController);
 
-    function CategoryListController(UserService, $location, $anchorScroll, $rootScope) {
+    function CategoryListController(UserService, $location, $anchorScroll, $rootScope, $http, $q) {
         var vm = this;
         vm.setZipCode = setZipCode;
         vm.submitted = false;
@@ -13,6 +13,34 @@
         vm.onChanged = onChanged;
         vm.currentUser = $rootScope.currentUser;
         vm.logout = logout;
+
+        function init() {
+            window.navigator.geolocation.getCurrentPosition(function(pos){
+                $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng='+pos.coords.latitude
+                    +','+pos.coords.longitude+'&sensor=true').then(function(res){
+                    if (res.data && res.data.results) {
+                        var results = res.data.results;
+                        var zip = extractFromAdress(results[0].address_components, "postal_code");
+                        console.log(zip);
+                        if (isValidUSZip(zip)) {
+                            vm.zip = zip;
+                        }
+                    }
+                });
+            })
+        }
+        init();
+
+        function extractFromAdress(components, type){
+            for (var i=0; i<components.length; i++) {
+                for (var j = 0; j < components[i].types.length; j++) {
+                    if (components[i].types[j] == type) {
+                        return components[i].long_name;
+                    }
+                }
+            }
+                return "";
+        }
 
         function logout() {
             $rootScope.currentUser = null;
